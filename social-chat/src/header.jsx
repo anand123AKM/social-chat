d3c76f0a04d10217393f12808ff60f15a1820e7c
anand maurya <anandkr.maurya10@gmail.com>
2024-04-29 11:06:37 +0530

    added

--- social-chat/src/header.jsx
+++ social-chat/src/header.jsx
@@ -11,10 +11,9 @@ const Header = ({tab, settab}) => {
        <header className={`${styles.divsty} p-3 text-bg-dark`}>
     <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap"></use></svg>
-        </a>
-
+         <h1 className={styles.heading} >SOCIAL CHAT</h1>
+    
+       
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" className="nav-link px-2 text-secondary ">Home</a></li>
             <li><a href="#" className="nav-link px-2 text-white">All Post</a></li>
